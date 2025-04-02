113b8fb0f64feaa9e404fa6084608f1d7db96718
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-02 14:40:48 -0300

    fix: adequação das rotas

--- EmprestimosFront/src/app/app.routes.ts
+++ EmprestimosFront/src/app/app.routes.ts
@@ -4,9 +4,11 @@ import {NgModule} from '@angular/core';
 import {
   DashboardEmprestimoComponent
 } from './components/dashboardEmprestimos/page/dashboard-emprestimo/dashboard-emprestimo.component';
+import {AuthComponent} from './components/auth/auth.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', component: AuthComponent},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'dashboard/:id/emprestimo', component: DashboardEmprestimoComponent},
 ];
