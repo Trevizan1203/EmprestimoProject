8520fad0f431c7ff52d9e76304e61bdd63278e33
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-02 18:08:48 -0300

    feat: toolbar para o dashboard de clientes

---
+++ EmprestimosFront/src/app/components/dashboardCliente/components/tools/tools.component.ts
@@ -0,0 +1,26 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-tools',
+  imports: [],
+  templateUrl: './tools.component.html',
+  styleUrl: './tools.component.css'
+})
+export class ToolsComponent {
+  @Output()
+  cliente: EventEmitter<void> = new EventEmitter();
+  @Output()
+  user: EventEmitter<void> = new EventEmitter();
+
+  constructor(private router: Router) {
+  }
+
+  areaCliente() {
+    this.cliente.emit();
+  }
+
+  logout() {
+    this.router.navigateByUrl('');
+  }
+}
