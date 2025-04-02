b3c53c94ed74bfbfed1961ecfd2b227562edd24b
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-02 14:41:17 -0300

    fix: integração da service do token na api de usuarios

--- CotacaoEmprestimos/src/main/java/Aplicacao/CotacaoEmprestimos/api/controller/UserController.java
+++ CotacaoEmprestimos/src/main/java/Aplicacao/CotacaoEmprestimos/api/controller/UserController.java
@@ -1,6 +1,9 @@
 package Aplicacao.CotacaoEmprestimos.api.controller;
 
+import Aplicacao.CotacaoEmprestimos.api.DTOs.LoginRequestDTO;
+import Aplicacao.CotacaoEmprestimos.api.DTOs.LoginResponseDTO;
 import Aplicacao.CotacaoEmprestimos.api.DTOs.UserDTO;
+import Aplicacao.CotacaoEmprestimos.services.TokenService;
 import Aplicacao.CotacaoEmprestimos.services.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +15,22 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final TokenService tokenService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TokenService tokenService) {
         this.userService = userService;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody UserDTO userDTO) {
         userService.createUser(userDTO);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        return ResponseEntity.ok(tokenService.login(loginRequest));
     }
 
 }
