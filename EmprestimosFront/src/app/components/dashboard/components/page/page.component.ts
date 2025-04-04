807a911d64fb640c08fae0b336dd241912a41b05
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-04 18:11:15 -0300

    feat: adequação do display da pagina

--- EmprestimosFront/src/app/components/dashboard/components/page/page.component.ts
+++ EmprestimosFront/src/app/components/dashboard/components/page/page.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {GraficoComponent} from '../graficos/pizza/grafico.component';
 import {HeaderComponent} from '../header/header.component';
+import {BarraComponent} from '../graficos/barra/barra.component';
 
 @Component({
   selector: 'app-page',
   imports: [
     GraficoComponent,
-    HeaderComponent
+    HeaderComponent,
+    BarraComponent
   ],
   templateUrl: './page.component.html',
   styleUrl: './page.component.css'
