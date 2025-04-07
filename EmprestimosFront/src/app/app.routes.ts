7357ce9f6795f1d71296de69e50854ba89391ac7
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-07 16:36:57 -0300

    feat: rota para area de dados

--- EmprestimosFront/src/app/app.routes.ts
+++ EmprestimosFront/src/app/app.routes.ts
@@ -6,12 +6,14 @@ import {
 } from './components/dashboardEmprestimos/page/dashboard-emprestimo/dashboard-emprestimo.component';
 import {AuthComponent} from './components/auth/auth.component';
 import {guardAutenticacaoGuard} from './guards/guard-autenticacao.guard';
+import {PageComponent} from './components/dashboard/components/page/page.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: AuthComponent},
   {path: 'dashboard', component: DashboardComponent, canActivate: [guardAutenticacaoGuard]},
   {path: 'dashboard/:id/emprestimo', component: DashboardEmprestimoComponent, canActivate: [guardAutenticacaoGuard]},
+  {path: 'info', component: PageComponent, canActivate: [guardAutenticacaoGuard]},
   {path: '**', redirectTo: 'login'}
 ];
 
