af6308448233dcba2c17b4d46773ec9ae645cf54
gustavo.silva <gustavo-trevizan@hotmail.com>
2025-04-07 08:31:44 -0300

    fix: correção dos dados do chart com lazy loading

--- EmprestimosFront/src/app/components/dashboard/components/graficos/barra/barra.component.ts
+++ EmprestimosFront/src/app/components/dashboard/components/graficos/barra/barra.component.ts
@@ -1,22 +1,37 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EmprestimoService} from '../../../../../services/API/emprestimo.service';
 import {EmprestimoChartModel} from '../../../../../models/emprestimo-chart-model';
 import {NotificationService} from '../../../../../services/notification.service';
 import {ChartConfiguration} from 'chart.js';
 import {BaseChartDirective} from 'ng2-charts';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-barra',
   imports: [
-    BaseChartDirective
+    BaseChartDirective,
+    NgIf
   ],
   templateUrl: './barra.component.html',
   styleUrl: './barra.component.css'
 })
-export class BarraComponent implements OnInit {
+export class BarraComponent{
   valoresMeses: number[] = [0, 0, 0, 0];
-  meses: number[] = [0, 0, 0, 0];
+  carregado: boolean = false
   //0: atual, 1: -1mes, 2: -2mes...
+
+  public barChartData: ChartConfiguration<'bar'>['data'] = {
+    datasets: [
+      {
+        data: [],
+        backgroundColor: "blue",
+        label: 'Valor Total Emprestado (R$)',
+        borderColor: "blue",
+        borderWidth: 1
+      }
+    ],
+    labels: ['Mês Anterior ao Retrasado', 'Mês Retrasado', 'Mês Passado', 'Mês Atual']
+  };
 
   constructor(private emprestimoService: EmprestimoService, private notificationService: NotificationService) {
     let hoje = new Date();
@@ -48,27 +63,17 @@ export class BarraComponent implements OnInit {
               break;
           }
         });
+        setTimeout(() => {
+          this.barChartData.datasets[0].data = this.valoresMeses;
+          this.carregado = true;
+        }, 500);
       },
       error: error => this.notificationService.showToast(error, 'danger')
     });
-    this.barChartData.datasets[0].data = this.valoresMeses;
   }
 
   public barChartLegend = true;
   public barChartPlugins = [];
-
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    datasets: [
-      {
-        data: [],
-        backgroundColor: "blue",
-        label: 'Valor Total Emprestado (R$)',
-        borderColor: "blue",
-        borderWidth: 1
-      }
-    ],
-    labels: ['Mês Anterior ao Retrasado', 'Mês Retrasado', 'Mês Passado', 'Mês Atual']
-  };
 
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: true,
@@ -79,9 +84,5 @@ export class BarraComponent implements OnInit {
       }
     }
   };
-
-  ngOnInit() {
-    this.barChartData.datasets[0].data = this.valoresMeses;
-  }
 
 }
